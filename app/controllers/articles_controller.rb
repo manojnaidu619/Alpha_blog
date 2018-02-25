088beba13f7584d9c1f0a12981c0291189b6ca92
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-02-25 11:55:15 +0530

    Added destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,18 @@ class ArticlesController < ApplicationController
      end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+      redirect_to articles_path
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
  private
   def article_params
     params.require(:article).permit(:title,:description)
