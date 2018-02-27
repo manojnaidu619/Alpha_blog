bf60e1a69414d14e207d1af3fc32db3084bdc2ac
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-02-27 20:09:13 +0530

    Added User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username , presence: true ,
+  length: {minimum: 3 ,maximum: 25},
+  uniqueness: {case_sensitive: false}
+
+  EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email , presence: true ,length: {maximum: 60},
+  format: {with: EMAIL_REGEX}
 end
