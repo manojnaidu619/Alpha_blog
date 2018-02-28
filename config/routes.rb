3ab773416a5282d3697777b80ee05b53f9c40ba3
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-02-28 18:03:01 +0530

    added user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'home#rootpage'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users ,except: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
