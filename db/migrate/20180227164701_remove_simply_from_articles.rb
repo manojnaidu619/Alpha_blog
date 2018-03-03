6b9eb3383b18d6c7b7dcefe89c68778ac744fb2f
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-03-03 18:52:34 +0530

    removed some migrations

--- db/migrate/20180227164701_remove_simply_from_articles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSimplyFromArticles < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :articles, :simply, :integer
-  end
-end
--- db/migrate/20180227170036_remove_user_id_from_articles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromArticles < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :articles, :user_id, :integer
-  end
-end
